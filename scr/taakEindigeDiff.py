548af99dcff380c1b1b05f4e54602b051f5563f9
isi <isidoor.pinilloesquivel@student.uantwerpen.be>
2022-11-29 16:18:04 +0100

    oefening2 written out in verslag

--- scr/taakEindigeDiff.py
+++ scr/taakEindigeDiff.py
@@ -8,22 +8,23 @@ import pandas as pd
 from plotnine import ggplot, geom_point, aes
 
 
-def grafiekMatrixExpA(l:"lijst van m" = [5,10,20,50,100]):
+def grafiekMatrixExpA(alleM:"lijst van m" = [5,10,20,50,100]):
     p = parametersOpgave(0)
     t = np.arange(0,p.looptijd, p.looptijd/200)
 
     df = pd.DataFrame({"time":[], "m":[], "n2expM":[]})
 
-    for m in l:
+    for m in alleM:
         par = parametersOpgave(m)
         for time in t:
             row = pd.Series([time,m, norm(expm(par.A()*time),2)],index = df.columns)
             df = df.append(row, ignore_index = True)
 
-    tmp = ggplot(df) + aes(x="time",y= "n2expM",color="m") + geom_point(size = 0.5)
-    tmp.save("oefening2.png")
+    plot = ggplot(df) + aes(x="time",y= "n2expM",color="m") + geom_point(size = 0.5)
+    plot.save("../verslag/oefening2.png",dpi = 300)
 
 
 
 
 if __name__ == "__main__":
+    grafiekMatrixExpA()
